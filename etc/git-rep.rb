a0e99665a90828cfe903997388a0ed771e66f669
Kent Nagata <ngtknt@me.com>
2015-11-03 14:04:38 +0900

    make formula decent :ok_woman:

--- etc/git-rep.rb
+++ etc/git-rep.rb
@@ -1,16 +1,12 @@
-# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                http://www.rubydoc.info/github/Homebrew/homebrew/master/frames
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
-class GitRep < Formula
-  desc ""
-  homepage ""
-  url "https://github.com/ngtk/git-rep/archive/v0.0.1.tar.gz"
-  version "0.0.1"
-  sha256 "86013a457836d37742a0b3cf67023139c6e23ae809a4664a0e28cc4dc414ee69"
+class GitReplaceText < Formula
+  desc "git sub-command to replace the text in the repository."
+  homepage "https://github.com/ngtk/git-replace-text"
+  url "https://github.com/ngtk/git-replace-text/archive/v#{version}.tar.gz"
+  version "0.0.2"
+  sha256 "663c77fc7d4779de81c2277d663f97fba6b365387a080bcee43caa0c1fdcad81"
 
   def install
-    system "make", "install", "PREFIX=#{prefix}"
+    system "make", "install", "prefix=#{prefix}"
   end
 
   test do
